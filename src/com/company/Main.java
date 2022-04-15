c4117e7f3e72c925cb5088321cfa9cf70606d3e3
DrPhannibal <mahdisouri0@gmail.com>
2022-04-16 01:01:56 +0430

    java_session_3

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte variable_byte=123;
+        short variable_short=12345;
+        int variable_int=1234567890;
+        long variable_long=1234567890123456789L;
+
+        float variable_float=1.2F;
+        double variable_double=1.3D;
+
+        char variable_ch='a';
+        String variable_st="hi";
+
+        boolean variable_bol=false;
+    }
+}
